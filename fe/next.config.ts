1937a465d9f975dff3288395c0fcef692af1c4ed
dinevsg <dinevsg@icloud.com>
2025-09-16 11:10:10 +0300

    fe edited

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,17 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // <-- bypass ESLint errors during production build
-  },
+    output: 'export',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
+    eslint: {
+        ignoreDuringBuilds: true, // <-- bypass ESLint errors during production build
+    },
 };
 
 export default nextConfig;
