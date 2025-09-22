7fbea680cafdb863bd339ba2d1599f81b8119066
dinevsg <dinevsg@icloud.com>
2025-09-23 02:32:01 +0300

    add roadmap.sh svg

--- fe/src/components/Footer.tsx
+++ fe/src/components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             <span className="sr-only">LinkedIn</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-9 h-9"
+              className="w-10 h-10"
               fill="currentColor"
               viewBox="0 0 64 64"
             >
@@ -41,7 +41,7 @@ export default function Footer() {
             <span className="sr-only">GitHub</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-8 h-8"
+              className="w-10 h-10 p-0.5"
               fill="currentColor"
               viewBox="0 0 64 64"
             >
@@ -50,20 +50,37 @@ export default function Footer() {
           </a>
 
           <a
-            href="http://x.com/dinevsg"
+            href="https://roadmap.sh/u/dinevsg"
             target="_blank"
             rel="noopener noreferrer"
             className="text-neutral-primary hover:text-indigo-500 transition"
           >
-            <span className="sr-only">X</span>
+            <span className="sr-only">roadmap</span>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-8"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"></path>
-            </svg>
+  className="w-10 h-10 p-2"
+  viewBox="0 0 283 283"
+  xmlns="http://www.w3.org/2000/svg"
+  role="img"
+  aria-hidden="true"
+>
+  <defs>
+    <mask id="roadmap-mask" maskUnits="userSpaceOnUse" x="0" y="0" width="283" height="283">
+      {/* Outer rounded box (white = visible in mask) */}
+      <path
+        d="M0 39C0 17.46 17.46 0 39 0h205c21.539 0 39 17.46 39 39v205c0 21.539-17.461 39-39 39H39c-21.54 0-39-17.461-39-39V39Z"
+        fill="white"
+      />
+      {/* 'r' shape (black = cut out in mask) */}
+      <path
+        d="M121.215 210.72c-1.867.56-4.854 1.12-8.96 1.68-3.92.56-8.027.84-12.32.84-4.107 0-7.84-.28-11.2-.84-3.174-.56-5.88-1.68-8.12-3.36s-4.014-3.92-5.32-6.72c-1.12-2.987-1.68-6.813-1.68-11.48v-84c0-4.293.746-7.933 2.24-10.92 1.68-3.173 4.013-5.973 7-8.4s6.626-4.573 10.92-6.44c4.48-2.053 9.24-3.827 14.28-5.32a106.176 106.176 0 0 1 15.68-3.36 95.412 95.412 0 0 1 16.24-1.4c8.96 0 16.053 1.773 21.28 5.32 5.226 3.36 7.84 8.96 7.84 16.8 0 2.613-.374 5.227-1.12 7.84-.747 2.427-1.68 4.667-2.8 6.72a133.1 133.1 0 0 0-12.04.56c-4.107.373-8.12.933-12.04 1.68s-7.654 1.587-11.2 2.52c-3.36.747-6.254 1.68-8.68 2.8v95.48zm45.172-22.4c0-7.84 2.426-14.373 7.28-19.6s11.48-7.84 19.88-7.84 15.026 2.613 19.88 7.84 7.28 11.76 7.28 19.6-2.427 14.373-7.28 19.6-11.48 7.84-19.88 7.84-15.027-2.613-19.88-7.84-7.28-11.76-7.28-19.6z"
+        fill="black"
+      />
+    </mask>
+  </defs>
+
+  {/* Draw the square filled by currentColor, but apply the mask so the 'r' is transparent */}
+  <rect width="283" height="283" fill="currentColor" mask="url(#roadmap-mask)" />
+</svg>
           </a>
         </span>
       </div>
--- fe/src/pages/HomePage.tsx
+++ fe/src/pages/HomePage.tsx
@@ -170,11 +170,11 @@ const Home: React.FC = () => {
             <p className="py-2 m-0 leading-7 text-neutral-secondary text-md xl:text-lg sm:pr-12 xl:pr-32 text-md">
               You can find insightful articles about Data, Machine Learning and Cloud technologies, where I share my thoughts, experiences, and the latest trends in these fields.
             </p>
-            <ul className="py-2 flex items-center gap-4 leading-6 text-neutral-main">
+            <ul className="py-2 flex items-center gap-6 leading-6 text-neutral-main">
               <li>
                 <a href="https://www.linkedin.com/in/sdinev/" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500 transition">
                   <span className="sr-only">LinkedIn</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10" fill="currentColor" viewBox="0 0 64 64">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-11 h-11" fill="currentColor" viewBox="0 0 64 64">
                     <path d="M40.227,12C51.145,12,52,12.854,52,23.773v16.453C52,51.145,51.145,52,40.227,52H23.773C12.855,52,12,51.145,12,40.227	V23.773C12,12.854,12.855,12,23.773,12H40.227z M25.029,43V26.728h-5.057V43H25.029z M22.501,24.401	c1.625,0,2.947-1.322,2.947-2.949c0-1.625-1.322-2.947-2.947-2.947c-1.629,0-2.949,1.32-2.949,2.947S20.87,24.401,22.501,24.401z M44,43v-8.925c0-4.382-0.946-7.752-6.067-7.752c-2.46,0-4.109,1.349-4.785,2.628H33.08v-2.223h-4.851V43h5.054v-8.05	c0-2.122,0.405-4.178,3.036-4.178c2.594,0,2.628,2.427,2.628,4.315V43H44z"></path>
                   </svg>
                 </a>
@@ -182,23 +182,59 @@ const Home: React.FC = () => {
               <li>
                 <a href="https://github.com/dinevsg" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500 transition">
                   <span className="sr-only">GitHub</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-9 h-9" fill="currentColor" viewBox="0 0 64 64">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-11 h-11" fill="currentColor" viewBox="0 0 64 64">
                     <path d="M32,10c12.15,0,22,9.85,22,22c0,9.768-6.369,18.045-15.179,20.916c0.002-0.008,0.006-0.021,0.006-0.021	s-1.485-0.696-1.453-1.938c0.035-1.367,0-4.556,0-5.727c0-2.01-1.272-3.434-1.272-3.434s9.977,0.112,9.977-10.533	c0-4.107-2.147-6.245-2.147-6.245s1.128-4.385-0.39-6.245c-1.701-0.184-4.749,1.626-6.05,2.472c0,0-2.062-0.846-5.492-0.846	c-3.43,0-5.492,0.846-5.492,0.846c-1.301-0.846-4.348-2.656-6.05-2.472c-1.518,1.86-0.39,6.245-0.39,6.245s-2.147,2.137-2.147,6.245	c0,10.645,9.977,10.533,9.977,10.533s-1.005,1.136-1.225,2.806c-0.696,0.236-1.721,0.528-2.549,0.528	c-2.165,0-3.812-2.105-4.416-3.078c-0.595-0.96-1.815-1.766-2.953-1.766c-0.749,0-1.115,0.375-1.115,0.803s1.05,0.727,1.743,1.521	c1.461,1.674,1.435,5.438,6.641,5.438c0.565,0,1.719-0.139,2.588-0.256c-0.005,1.185-0.007,2.436,0.012,3.167	c0.031,1.242-1.453,1.938-1.453,1.938s0.004,0.012,0.006,0.021C16.369,50.045,10,41.768,10,32C10,19.85,19.85,10,32,10z"></path>
                   </svg>
                 </a>
               </li>
               <li>
-                <a href="http://x.com/dinevsg" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500 transition">
-                  <span className="sr-only">X</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-7" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"></path>
-                  </svg>
+                <a href="http://roadmap.sh/u/dinevsg" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500 transition">
+                  <span className="sr-only">roadmap</span>
+                    <svg
+  className="w-10 h-10 p-1.5"
+  viewBox="0 0 283 283"
+  xmlns="http://www.w3.org/2000/svg"
+  role="img"
+  aria-hidden="true"
+>
+  <defs>
+    <mask id="roadmap-mask" maskUnits="userSpaceOnUse" x="0" y="0" width="283" height="283">
+      {/* Outer rounded box (white = visible in mask) */}
+      <path
+        d="M0 39C0 17.46 17.46 0 39 0h205c21.539 0 39 17.46 39 39v205c0 21.539-17.461 39-39 39H39c-21.54 0-39-17.461-39-39V39Z"
+        fill="white"
+      />
+      {/* 'r' shape (black = cut out in mask) */}
+      <path
+        d="M121.215 210.72c-1.867.56-4.854 1.12-8.96 1.68-3.92.56-8.027.84-12.32.84-4.107 0-7.84-.28-11.2-.84-3.174-.56-5.88-1.68-8.12-3.36s-4.014-3.92-5.32-6.72c-1.12-2.987-1.68-6.813-1.68-11.48v-84c0-4.293.746-7.933 2.24-10.92 1.68-3.173 4.013-5.973 7-8.4s6.626-4.573 10.92-6.44c4.48-2.053 9.24-3.827 14.28-5.32a106.176 106.176 0 0 1 15.68-3.36 95.412 95.412 0 0 1 16.24-1.4c8.96 0 16.053 1.773 21.28 5.32 5.226 3.36 7.84 8.96 7.84 16.8 0 2.613-.374 5.227-1.12 7.84-.747 2.427-1.68 4.667-2.8 6.72a133.1 133.1 0 0 0-12.04.56c-4.107.373-8.12.933-12.04 1.68s-7.654 1.587-11.2 2.52c-3.36.747-6.254 1.68-8.68 2.8v95.48zm45.172-22.4c0-7.84 2.426-14.373 7.28-19.6s11.48-7.84 19.88-7.84 15.026 2.613 19.88 7.84 7.28 11.76 7.28 19.6-2.427 14.373-7.28 19.6-11.48 7.84-19.88 7.84-15.027-2.613-19.88-7.84-7.28-11.76-7.28-19.6z"
+        fill="black"
+      />
+    </mask>
+  </defs>
+
+  {/* Draw the square filled by currentColor, but apply the mask so the 'r' is transparent */}
+  <rect width="283" height="283" fill="currentColor" mask="url(#roadmap-mask)" />
+</svg>
+                </a>
+              </li>
+               <li>
+                <a href="https://app.daily.dev/dinevsg" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500 transition">
+                  <span className="sr-only">GitHub</span>
+                    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-10 h-10"
+      viewBox="0 0 256 144"
+      fill="currentColor"
+    >
+      <path d="M219.832837,71.6995129 L191.573071,43.3805814 L205.69559,15.0714884 L251.612188,61.0842182 C257.462604,66.9468836 257.462604,76.4521422 251.612188,82.3148081 L195.097565,138.947752 C189.247148,144.810417 179.761748,144.810417 173.911332,138.947752 C168.060915,133.085086 168.060915,123.579827 173.911332,117.717162 L219.832837,71.6995129 Z"></path>
+      <path d="M173.921862,4.39576953 C179.772279,-1.46689601 189.260133,-1.46443637 195.11055,4.39822916 L205.706121,15.0159838 L82.0886685,138.892247 C76.238252,144.754913 66.7503974,144.752453 60.8999803,138.889788 L50.3044093,128.272033 L173.921862,4.39576953 Z M120.941551,43.3299961 L99.7504086,64.5655051 L71.4906426,36.2465737 L36.167163,71.6440084 L64.4269285,99.96294 L50.3044093,128.272033 L4.38781264,82.2593032 C-1.46260421,76.3966377 -1.46260421,66.8913792 4.38781264,61.0287132 L60.8999803,4.39822916 C66.7503974,-1.46443637 76.238252,-1.46689601 82.0886685,4.39576953 L120.941551,43.3299961 Z"></path>
+    </svg>
                 </a>
               </li>
               <li>
                 <a href="mailto:sd@sdinev.com" className="hover:text-indigo-500 transition">
                   <span className="sr-only">Email</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-7">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10 p-1">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M22 7.535v9.465a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9.465l9.445 6.297l.116 .066a1 1 0 0 0 .878 0l.116 -.066l9.445 -6.297z" />
                     <path d="M19 4c1.08 0 2.027 .57 2.555 1.427l-9.555 6.37l-9.555 -6.37a2.999 2.999 0 0 1 2.354 -1.42l.201 -.007h14z" />
