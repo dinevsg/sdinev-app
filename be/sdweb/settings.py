ed1795d6595c9cc8a2e6c2d55c9f8e748ef48e30
dinevsg <dinevsg@icloud.com>
2025-09-24 10:31:57 +0300

    change s3 bucketname

--- be/sdweb/settings.py
+++ be/sdweb/settings.py
@@ -201,7 +201,7 @@ if DEBUG:
 else:
     # Production (Supabase S3)
     MEDIA_ROOT = None  # not used
-    MEDIA_URL = f"https://tlveapgzoxcunhpwzxup.supabase.co/storage/v1/object/public/{SUPABASE_S3_BUCKET_NAME}/"
+    MEDIA_URL = f"https://tlveapgzoxcunhpwzxup.supabase.co/storage/v1/object/public/sdinev-media/"
 
 
 # Default primary key field type
